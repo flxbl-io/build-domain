4d4a1de8bf6e2cc021916a7b22e4839331d2e41e
azlam-salam <azlam@flxbl.io>
2026-01-19 17:03:55 +1100

    chore: add new shared module type definitions (changelog, github-comments, github-context)

--- dist/shared/index.d.ts
+++ dist/shared/index.d.ts
@@ -1,1 +1,4 @@
 export { fetchGitHubToken, AuthTokenResponse, ErrorResponse } from './github-token';
+export { getGitHubContext, parseRepository, GitHubContext } from './github-context';
+export { findComment, upsertComment, Comment } from './github-comments';
+export { generateChangelog, formatChangelogComment, ChangelogOptions, ChangelogResult, ReleaseStatus } from './changelog';
